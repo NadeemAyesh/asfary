73e10704872d63771f9b1c6f0463f027030a9e1e
n.ayesh <n.ayesh@newline.ps>
2020-05-29 21:43:09 +0300

    adding all

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -109,12 +109,43 @@ $(document).ready(function () {
         });
     }
 
+    // gallery-slider Slider
+    if ($('.gallery-slider .owl-carousel').length > 0) {
+        $('.gallery-slider .owl-carousel').owlCarousel({
+            // rtl: true,
+            loop: true,
+            autoplay: true,
+            margin: 0,
+            responsiveClass: true,
+            nav: true,
+            dots: true,
+            navText: ['<i class="fa fa-arrow-left fa-fw"></i>', '<i class="fa fa-arrow-right fa-fw"></i>'],
+            items: 1,
+            dotsContainer: '#carousel-custom-dots'
+        });
+
+        $('.owl-dot').click(function () {
+            $('.gallery-slider .owl-carousel').trigger('to.owl.carousel', [$(this).index(), 300]);
+        });
+    }
+
     $('.trip-item').hover(function () {
         $(this).find('.trip-caption > p').slideDown();
 
         $('.trip-item').mouseleave(function () {
             $(this).find('.trip-caption > p').slideUp();
         });
+    });
+
+    $('.collapse-item h5').on('click', function () {
+        if ($(this).find('i').hasClass('fa-minus')) {
+            $(this).find('i').addClass('fa-plus');
+            $(this).find('i').removeClass('fa-minus');
+        } else {
+            $(this).find('i').removeClass('fa-plus');
+            $(this).find('i').addClass('fa-minus');
+        }
+        $(this).parent().find('.collapse-it').slideToggle();
     });
 
 });
